a2c97f1ca91daff0c49eb8318b128d596b191f04
rehnuma-lamia <rehnuma.lamia@gmail.com>
2020-01-23 12:53:41 +0100

    task-2 Home component,Post-details component,Comments component created

--- https-demo/src/app/app.module.ts
+++ https-demo/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { PostDetailsComponent } from './post-details/post-details.component';
+import { CommentsComponent } from './comments/comments.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    PostDetailsComponent,
+    CommentsComponent
   ],
   imports: [
     BrowserModule,
